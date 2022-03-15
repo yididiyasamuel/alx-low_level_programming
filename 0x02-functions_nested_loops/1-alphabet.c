5ca1d5b8e10581da52f349ad29742704f2ea6dd0
Yididiya Samuel <ysamuel2311@gmail.com>
2022-03-15 09:52:25 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ ** print_alphabet - print alphabet in lowercase.
+ **/
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
